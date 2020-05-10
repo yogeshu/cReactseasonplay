631a2b3a3caee614762dbca05cd5a7d17c4be6ed
yogeshu <yogeshbhavsar1994@gmail.com>
2020-05-10 14:21:45 +0530

    The Season Disply component added

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const SeasonDisplay = () =>{
+    return <div> I am SeasonDisplay Component Of APP </div>
+}
+ 
+export default SeasonDisplay
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React , { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class App extends Component {
+
+    render() { 
+        window.navigator.geolocation.getCurrentPosition(
+            (position) =>  console.log(position),
+            (err) => console.log(err)
+        )
+        return (
+            <div> 
+                Latitude
+            </div>
+          );
+    }
+}
+ 
+export default App;
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
