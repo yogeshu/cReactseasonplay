44b9ff0d0bf8570bd48572219fe8cc8d3f5d104b
yogeshu <yogeshbhavsar1994@gmail.com>
2020-05-10 17:06:05 +0530

    css files for background and style

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-
+import "./App.css";
 const seasonConfig = {
- summer: {
-     text : "lets hit the beach",
-     iconName: 'sun'
- },
- winter: {
-     text: "Brr! its chiilly",
-     iconName:  "snowflake"
- }
-}
+  summer: {
+    text: "lets hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Brr! its chiilly",
+    iconName: "snowflake"
+  }
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -20,14 +20,14 @@ const getSeason = (lat, month) => {
 };
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const { text , iconName} = seasonConfig[season]
- return( <div> 
-
-     <i className={  `${iconName} icon` } />
-    <p> {text} </p>
-      <i className= { `${iconName} icon ` }/>
-        </div>  
-  )
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div className= {  ` display-season ${season}` }>
+      <i className={` icon-left massive ${iconName} icon`} />
+      <p> {text} </p>
+      <i className={` icon-right massive  ${iconName} icon `} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
---
+++ src/TheLoader.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Loader = () => {
+    return  (
+    <div>
+
+
+    </div>                )
+  }
+export default Loader;
